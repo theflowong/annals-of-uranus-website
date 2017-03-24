d68cb86568d5c5a1f2617c94be1fdf7d158c11cb
Florence Wong <florencewong1020@gmail.com>
2017-03-24 16:57:13 -0500

    experiment with js animations

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+
+/*
+(function() {
+
+  // Get buttons
+  var planetOne = document.getElementById('planet-1');
+  var planetTwo = document.getElementById('planet-2');
+
+  // rest of code goes here...
+
+}());
+
+*/
+
+// canvas... but do canvas buttons work?
+// var canvas = document.getElementById('stage');
+
+/* ANIMATIONS: learning
+// animations
+function animate(something) {
+  requestAnimationFrame(animate);
+  // animate something...
+}
+
+// Start the animation
+var requestID = requestAnimationFrame(animate);
+
+
+// Cancelling animation frames
+cancelAnimationFrame(requestID);
+*/
